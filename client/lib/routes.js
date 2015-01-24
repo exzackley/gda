2b2b54b6a05434b16c50fada7acfb77e2fc185a4
Jeff Xiong <gigix1980@gmail.com>
2015-01-24 20:52:26 +0300

    translation for navbar and header

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -1,3 +1,9 @@
+Router.route('gda/cn', function() {
+    i18n.setLanguage('cn');
+    this.render('index');
+});
+
 Router.route('gda', function () {
+    i18n.setLanguage('en');
     this.render('index');
 });
---
+++ common/0_i18n.js
@@ -0,0 +1,37 @@
+i18n.map('en', {
+    site: {
+        name: 'Global Drivers Alliance'
+    },
+    nav: {
+        why: 'Why?',
+        who: 'Who’s behind?',
+        how_to_help: 'How customers can help?',
+        contact: 'Contact'
+    },
+    header: {
+        title: 'Drive for Uber?',
+        left_p1: 'We built Uber into the company that it is today. We made sacrifices and risked our livelihoods. How has Uber has repaid us? By attacking our incomes, controlling every aspect of our work, and flooding the market while throwing many devoted drivers off the platform.',
+        left_p2: 'We therefore propose to form an independent organization of drivers to hold Uber true to its original vision and to protect our work and lives – an organization run exclusively and democratically by drivers from every Uber city in the world.',
+        right_p1: 'We know this is not a new idea. On this site, we are merely trying to gather everyone together so that we can all formally unite in a democratic, transparent and orderly fashion. No outside organization or union is involved.',
+        right_p2: 'We – the drivers you see below – are not putting ourselves up as leaders. Leaders must be chosen democratically by Uber drivers. We are simply proposing a process to get things started. We hope you will join us.'
+    }
+});
+
+i18n.map('cn', {
+    site: {
+        name: '全球司机联盟'
+    },
+    nav: {
+        why: '为什么？',
+        who: '我们是谁？',
+        how_to_help: '如何帮忙？',
+        contact: '联系我们'
+    },
+    header: {
+        title: '你给优步开车吗?',
+        left_p1: '是我们这些司机成就了优步的辉煌。给优步开车，我们做出了牺牲，承受了没有社保医保的风险。优步回报我们的是什么？不断打压我们的收入，控制我们工作的每个细节，把众多司机从这个平台赶走。',
+        left_p2: '所以，我们提议成立一个独立的、属于司机自己的组织，来督促优步兑现最初承诺的愿景，保障我们自己的工作与生活。这个组织将由来自各个城市的优步司机自主且民主地运营和管理。',
+        right_p1: '俗话说，众人拾柴火焰高。在这个网站上，我们只是尝试把各个城市里的优步司机连接起来，然后我们就可以民主、透明、有序地正式组织活动。这里不会涉及任何外部社会组织或劳工组织。',
+        right_p2: '我们——在下面你可以看到发起这个网站的司机们——并不把自己当成领导者。各地优步司机组织的领导者必须由当地的优步司机民主地选出。我们只是提议发起这样一个进程。我们希望你加入我们。'
+    }
+});
